86ce62717749e66700f8fc604a59cdb653efb651
hussssainnnn <muhammadhussainx7@gmail.com>
2025-09-22 12:32:14 +0530

    Add files via upload

---
+++ practiceproblem1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+ string name;
+int age;
+
+
+cout<<"Enter a name :";
+cin>> name;
+
+   
+cout<<"Enter your age :";
+cin>> age;
+
+
+cout<<"My name is " <<name<<  " and I am " <<age <<" years old" <<endl;
+
+
+
+
+
+
+
+
+    return 0;
+}
