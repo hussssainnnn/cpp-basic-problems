9c832e5391c84462bc1c0e50629a3a37b2a30a55
hussssainnnn <muhammadhussainx7@gmail.com>
2025-09-22 13:38:21 +0530

    Add files via upload

---
+++ oddevnprac.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int n;
+
+cout<<"Enter a number:";
+cin>>n;
+
+
+if (n%2==0)
+{
+    cout<<"The number is even";
+}
+else 
+cout<<"The Number is odd";
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
