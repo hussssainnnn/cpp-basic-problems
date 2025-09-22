7dd1aa3fd52e751cf7599614eee4d211a1d6db85
hussssainnnn <muhammadhussainx7@gmail.com>
2025-09-22 15:27:41 +0530

    Add files via upload

---
+++ vote.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int age;
+
+cout<<"Enter age: ";
+cin>>age;
+
+if (age>=18)
+{
+  cout<<"Can vote";
+}
+else 
+{
+    cout<<"Cannot vote";
+}
+
+
+
+
+
+
+
+    return 0;
+}
