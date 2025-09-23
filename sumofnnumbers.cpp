795a85bd338ca629b0980b84123456d3939cf976
hussssainnnn <muhammadhussainx7@gmail.com>
2025-09-23 13:54:48 +0530

    Add files via upload

---
+++ sumofnnumbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+
+
+int N,sum=0;
+
+cout<<"Enter a number:";
+cin>>N;
+
+for (int i = 1; i <=N ; i++)
+{
+    sum+=i;
+}
+
+cout<<"Sum = " <<sum<<endl;
+
+
+
+
+    return 0;
+}
