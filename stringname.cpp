5e617337284afde0f9f27f78870fe48e30918faf
hussssainnnn <muhammadhussainx7@gmail.com>
2025-09-23 16:44:10 +0530

    Add files via upload

---
+++ stringname.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+string name, age;
+cout<<"Enter name:";
+
+
+
+cin>>name;
+cout<<"Enter age:";
+cin>>age;
+
+
+cout<<"My name is "<<name<< " and I am " <<age <<" years old"<<endl;
+return 0;
+}
