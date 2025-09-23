1677cb4d57af1b82359a37d80408c336d713268d
hussssainnnn <muhammadhussainx7@gmail.com>
2025-09-23 16:34:33 +0530

    Add files via upload

---
+++ negativepositivezero.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int n;
+
+
+cout<<"Enter a number:";
+cin>>n;
+
+if (n>0){
+cout<<"Number is positive";
+}
+else if (n==0)
+{
+    cout<<"Number is zero";
+}
+else
+cout<<"Number is negative";
+
+
+
+
+return 0;
+
+
+
+
+
+
+
+
+    return 0;
+}
