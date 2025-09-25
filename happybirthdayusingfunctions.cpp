bb18e879db7464fd89fd979235dd8beb873c829c
hussssainnnn <muhammadhussainx7@gmail.com>
2025-09-25 23:12:26 +0530

    Add files via upload

---
+++ happybirthdayusingfunctions.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void happyBirthday();
+
+
+int main(){
+
+
+happyBirthday();
+
+    return 0;
+
+}
+
+void happyBirthday(){
+cout<<"Happy Birthday to you!\n";
+cout<<"Happy Birthday to you!\n";
+cout<<"Happy Birthday to dear ABC!\n";
+cout<<"Happy Birthday to you!\n";
+}
